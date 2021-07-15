0fd72ef477e2d28896f45a2bb4ec4223f4b2ec0a
HallexCosta <hallex.costa@hotmail.com>
2021-07-15 19:03:55 -0300

    chore(server): require dotenv/config on mocharc and mocharc-ci

--- .mocharc.js
+++ .mocharc.js
@@ -2,13 +2,13 @@ module.exports = {
   diff: true,
   color: true,
   exit: true,
-  extension: ["ts"],
-  package: "./package.json",
-  reporter: "landing",
-  file: ["__tests__/teardown.ts"],
-  require: ["ts-node/register", "tsconfig-paths/register"],
+  extension: ['ts'],
+  package: './package.json',
+  reporter: 'landing',
+  file: ['__tests__/teardown.ts'],
+  require: ['dotenv/config', 'ts-node/register', 'tsconfig-paths/register'],
   timeout: 0,
-  spec: ["__tests__/unit/**/*.spec.ts"],
-  ui: "bdd",
-  watchFiles: ["__tests__"],
-};
+  spec: ['__tests__/unit/**/*.spec.ts'],
+  ui: 'bdd',
+  watchFiles: ['__tests__']
+}
