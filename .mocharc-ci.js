6efc5d2803c979b0edfda7b10d5745bcb07d32ab
HallexCosta <hallex.costa@hotmail.com>
2021-07-15 16:10:45 -0300

    chore(server): add .mocharc-ci

---
+++ .mocharc-ci.js
@@ -0,0 +1,14 @@
+module.exports = {
+  diff: true,
+  color: true,
+  exit: true,
+  extension: ["ts"],
+  package: "./package.json",
+  reporter: "spec",
+  file: ["__tests__/teardown.ts"],
+  require: ["ts-node/register", "tsconfig-paths/register"],
+  timeout: 0,
+  spec: ["__tests__/unit/**/*.spec.ts"],
+  ui: "bdd",
+  exclude: ["__tests__/unit/isolate/*.spec.ts"],
+};
