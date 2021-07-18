c1b914f7a7c6a2fd6049452ca34c3f6d15e1dc1b
HallexCosta <hallex.costa@hotmail.com>
2021-07-18 20:26:51 -0300

    test(server): add Incident

---
+++ __tests__/unit/entities/Incident.spec.ts
@@ -0,0 +1,24 @@
+import { expect } from 'chai'
+
+import { Entity, Incident } from '@entities'
+
+import { incident } from '../fakes/stubs'
+
+describe('Incident Entity', () => {
+  it('Should be able to create a new Incident without the values of id, created_at, updated_at', () => {
+    const expected = new Incident({
+      name: incident.name,
+      coast: incident.coast,
+      description: incident.description
+    })
+
+    expect(expected).to.be.instanceOf(Entity)
+    expect(incident.id).to.be.lengthOf(36)
+  })
+
+  it('Should be able to create a new Incident with the values of id, created_at, updated_at', () => {
+    const expected = new Incident(incident)
+
+    expect(expected).to.be.instanceOf(Entity)
+  })
+})
