d728a91f7c909edf91019f3ad9a5763ee16bfef2
HallexCosta <hallex.costa@hotmail.com>
2021-07-19 14:09:55 -0300

    feat(server): add CreateDonors migration

---
+++ src/database/migrations/1626672983560-CreateDonors.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateDonors1626672983560 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'donors',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true
+          },
+          {
+            name: 'password',
+            type: 'varchar'
+          },
+          {
+            name: 'phone',
+            type: 'varchar'
+          },
+          {
+            name: 'created_at',
+            type: 'date',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'date',
+            default: 'now()'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('donors')
+  }
+}
