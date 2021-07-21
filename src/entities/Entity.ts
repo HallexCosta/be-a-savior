750d614ba141a21b770a6b60391fdafbe4a3a522
HallexCosta <hallex.costa@hotmail.com>
2021-07-21 15:05:08 -0300

    chore(entities): fix typeorm create instance when start connection

--- src/entities/Entity.ts
+++ src/entities/Entity.ts
@@ -1,29 +1,29 @@
+import { PrimaryColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm'
 import { v4 as uuid } from 'uuid'
 
-import { Override } from '@common'
-
-export type PartialEntity<T> = Override<T, Partial<Entity<T>>>
-
-export abstract class Entity<EntityType> {
+export abstract class Entity {
+  @PrimaryColumn()
   public readonly id: string
+
+  @CreateDateColumn()
   public readonly created_at: Date
+
+  @UpdateDateColumn()
   public readonly updated_at: Date
 
-  public constructor(props: PartialEntity<EntityType>) {
-    Object.assign(this, props)
-
-    if (!props.id) {
+  public constructor() {
+    if (!this.id) {
       this.id = uuid()
     }
 
-    if (!props.created_at) {
-      this.created_at = new Date()
+    const date = new Date()
+
+    if (!this.created_at) {
+      this.created_at = date
     }
 
-    if (!props.updated_at) {
-      this.updated_at = new Date()
+    if (!this.updated_at) {
+      this.updated_at = date
     }
-
-    Object.freeze(this)
   }
 }
