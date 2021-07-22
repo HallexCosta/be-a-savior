7bf3093283308b345c1e18f48d28d9909e1e21db
HallexCosta <hallex.costa@hotmail.com>
2021-07-22 10:39:23 -0300

    chore(repositories): add func findByEmail to DonorsRepository

--- src/repositories/DonorsRepository.ts
+++ src/repositories/DonorsRepository.ts
@@ -3,4 +3,10 @@ import { EntityRepository, Repository } from 'typeorm'
 import { Donor } from '@entities'
 
 @EntityRepository(Donor)
-export class DonorsRepository extends Repository<Donor> {}
+export class DonorsRepository extends Repository<Donor> {
+  async findByEmail(email: string): Promise<Donor> {
+    return await this.findOne({
+      email
+    })
+  }
+}
