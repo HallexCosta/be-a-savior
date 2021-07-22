85cbaefc47cccdf9e79635cf8e334ce7e27ec37f
HallexCosta <hallex.costa@hotmail.com>
2021-07-22 10:45:15 -0300

    feat(controllers): add CreateDonorController

---
+++ src/controllers/CreateDonorController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+
+import { CreateDonorService } from '@services'
+
+export class CreateDonorController {
+  public async handle(request: Request, response: Response): Promise<Response> {
+    const { name, email, password, phone } = request.body
+
+    const service = new CreateDonorService()
+
+    const donor = await service.execute({
+      name,
+      email,
+      password,
+      phone
+    })
+
+    return response.json(donor)
+  }
+}
