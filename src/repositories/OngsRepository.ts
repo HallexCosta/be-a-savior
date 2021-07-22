b661600031eb69c153aea126235724982cb68dbd
HallexCosta <hallex.costa@hotmail.com>
2021-07-22 10:43:07 -0300

    chore(repositories): add func findByEmail to OngsRepository

--- src/repositories/OngsRepository.ts
+++ src/repositories/OngsRepository.ts
@@ -3,4 +3,10 @@ import { EntityRepository, Repository } from 'typeorm'
 import { Ong } from '@entities'
 
 @EntityRepository(Ong)
-export class OngsRepository extends Repository<Ong> {}
+export class OngsRepository extends Repository<Ong> {
+  async findByEmail(email: string): Promise<Ong> {
+    return await this.findOne({
+      email
+    })
+  }
+}
