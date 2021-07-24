3107bd8f243b8ece53b9bacb8e60f1c1b3504934
HallexCosta <hallex.costa@hotmail.com>
2021-07-24 01:08:55 -0300

    chore(routes): add route GET /donors/:id

--- src/routes/donors.routes.ts
+++ src/routes/donors.routes.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express'
 
-import { CreateDonorController, ListDonorsController } from '@controllers'
+import {
+  CreateDonorController,
+  ListDonorsController,
+  ListDonorController
+} from '@controllers'
 
 const createDonorController = new CreateDonorController()
 const listDonorsController = new ListDonorsController()
+const listDonorController = new ListDonorController()
 
 const routes = Router()
 
 routes.post('/', createDonorController.handle)
 routes.get('/', listDonorsController.handle)
+routes.get('/:id', listDonorController.handle)
 
 export { routes }
