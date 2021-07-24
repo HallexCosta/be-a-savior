98e10b28b1e75b62c934d9a6d7d96b24efee124a
HallexCosta <hallex.costa@hotmail.com>
2021-07-24 01:05:50 -0300

    feat(controllers): add ListDonorController

---
+++ src/controllers/ListDonorController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express'
+
+import { ListDonorService } from '@services'
+
+export class ListDonorController {
+  public async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params
+
+    const service = new ListDonorService()
+
+    const donor = await service.execute({ id })
+
+    return response.json(donor)
+  }
+}
