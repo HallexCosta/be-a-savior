bcd35c909a26d076b0054d23b37a97c2ad6c32fe
HallexCosta <hallex.costa@hotmail.com>
2021-07-25 20:01:36 -0300

    feat(controllers): add CreateOngController

---
+++ src/controllers/CreateOngController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+
+import { CreateOngService } from '@services'
+
+export class CreateOngController {
+  public async handle(request: Request, response: Response): Promise<Response> {
+    const { name, email, password, phone } = request.body
+
+    const service = new CreateOngService()
+
+    const user = await service.execute({
+      name,
+      email,
+      password,
+      phone
+    })
+
+    return response.status(201).json(user)
+  }
+}
