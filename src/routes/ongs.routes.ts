50fb72b66f18bd2709c30f9cfe7d6203409fe7a6
HallexCosta <hallex.costa@hotmail.com>
2021-07-25 20:52:53 -0300

    chore(routes): use ListOngsController

--- src/routes/ongs.routes.ts
+++ src/routes/ongs.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 
-import { CreateOngController } from '@controllers'
+import { CreateOngController, ListOngsController } from '@controllers'
 
 const createOngController = new CreateOngController()
+const listOngsController = new ListOngsController()
 
 const routes = Router()
 
 routes.post('/', createOngController.handle)
+routes.get('/', listOngsController.handle)
 
 export { routes }
