1c93837382d251ebfdbd826dfa517f7b858d4cce
HallexCosta <hallex.costa@hotmail.com>
2021-07-25 20:48:17 -0300

    test(integration): add list ongs GET /ongs

--- __tests__/integration/ongs.routes.spec.ts
+++ __tests__/integration/ongs.routes.spec.ts
@@ -21,7 +21,7 @@ describe('Ongs Routes', () => {
       phone: '(99) 99999-9999'
     }
 
-    const response = await request(app).post('/donors').send(body)
+    const response = await request(app).post('/ongs').send(body)
     const expected = response.body
 
     id = expected.id
@@ -30,5 +30,14 @@ describe('Ongs Routes', () => {
     expect(expected).to.have.property('id')
     expect(expected).to.have.property('created_at')
     expect(expected).to.have.property('updated_at')
+  })
+
+  it('Should be able list Ongs GET (/ongs)', async () => {
+    const response = await request(app).get('/ongs')
+    const expected = response.body
+
+    expect(expected[0]).to.have.property('id')
+    expect(expected[0]).to.have.property('created_at')
+    expect(expected[0]).to.have.property('updated_at')
   })
 })
