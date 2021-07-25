1c4e91b47bbde0c4e0d8f82e7e4764d2e8c443cc
HallexCosta <hallex.costa@hotmail.com>
2021-07-25 19:50:51 -0300

    test(integration): add route POST /ongs should create an Ong

---
+++ __tests__/integration/ongs.routes.spec.ts
@@ -0,0 +1,34 @@
+import dirty from 'dirty-chai'
+import request from 'supertest'
+import { expect, use } from 'chai'
+
+import { app } from '@app'
+
+import { createTestingConnection } from './stubs'
+
+use(dirty)
+
+describe('Ongs Routes', () => {
+  let id: string
+
+  before(async () => await createTestingConnection())
+
+  it('Should be able to create new Ong POST (/ongs)', async () => {
+    const body = {
+      name: 'Some a name',
+      email: 'asome@hotmail.com',
+      password: 'some123',
+      phone: '(99) 99999-9999'
+    }
+
+    const response = await request(app).post('/donors').send(body)
+    const expected = response.body
+
+    id = expected.id
+
+    expect(expected).to.not.be.undefined()
+    expect(expected).to.have.property('id')
+    expect(expected).to.have.property('created_at')
+    expect(expected).to.have.property('updated_at')
+  })
+})
