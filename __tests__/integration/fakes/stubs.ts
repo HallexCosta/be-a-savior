a5ffc320f8ec34c618231953fbef21171c027d22
HallexCosta <hallex.costa@hotmail.com>
2021-07-26 12:37:56 -0300

    chore(fakes): add func to create fake ong

--- __tests__/integration/fakes/stubs.ts
+++ __tests__/integration/fakes/stubs.ts
@@ -1,5 +1,7 @@
 import { v4 as uuid } from 'uuid'
 import { Connection, createConnection } from 'typeorm'
+import { SuperTest, Test } from 'supertest'
+import { Ong } from '@entities'
 
 export async function createTestingConnection(): Promise<Connection> {
   return await createConnection({
@@ -15,4 +17,17 @@ export async function createTestingConnection(): Promise<Connection> {
       entitiesDir: 'src/entities'
     }
   })
+}
+
+export async function createFakeOng(app: SuperTest<Test>): Promise<Ong> {
+  const body = {
+    name: 'Some a name',
+    email: 'asome@hotmail.com',
+    password: 'some123',
+    phone: '(99) 99999-9999'
+  }
+
+  const response = await app.post('/ongs').send(body)
+
+  return response.body
 }
