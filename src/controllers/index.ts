789f0d2390b8371bdf4558b2a5147f46148089c1
HallexCosta <hallex.costa@hotmail.com>
2021-07-25 21:01:07 -0300

    feat(controllers): export ListOngController

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,6 @@
 export * from './CreateOngController'
 export * from './ListOngsController'
+export * from './ListOngController'
 
 export * from './CreateDonorController'
 export * from './ListDonorsController'
