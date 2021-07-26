bb6b72a004f2302cdb3d8cc00cb1c2bdaaab37b2
HallexCosta <hallex.costa@hotmail.com>
2021-07-26 12:49:40 -0300

    test(integration): add incidents.routes

---
+++ __tests__/integration/incidents.routes.spec.ts
@@ -0,0 +1,50 @@
+import dirty from 'dirty-chai'
+import request from 'supertest'
+import { expect, use } from 'chai'
+
+import { app } from '@app'
+
+import { createFakeOng, createTestingConnection } from './fakes/stubs'
+
+use(dirty)
+
+describe('Incidents Routes', () => {
+  let id: string
+
+  before(async () => await createTestingConnection())
+
+  it('Should be able to create new Incident POST (/incidents)', async () => {
+    const ong = await createFakeOng(request(app))
+
+    const body = {
+      name: 'Some a name',
+      coast: 100.5,
+      description: 'This is a description',
+      ong_id: ong.id
+    }
+
+    const response = await request(app).post('/incidents').send(body)
+    const expected = response.body
+
+    id = expected.id
+
+    expect(expected).to.not.be.undefined()
+    expect(expected).to.have.property('id')
+    expect(expected).to.have.property('created_at')
+    expect(expected).to.have.property('updated_at')
+  })
+
+  it('Should be able to throw an error if ong_id is fake POST (/incidents)', async () => {
+    const body = {
+      name: 'Some a name',
+      coast: 100.5,
+      description: 'This is a description',
+      ong_id: 'this is fake ong id'
+    }
+
+    const response = await request(app).post('/incidents').send(body)
+    const expected = response.body
+
+    expect(expected).to.have.property('error')
+  })
+})
