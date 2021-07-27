8ca3aeec3b29afe5dc352021f18ce3dc58eeaff7
HallexCosta <hallex.costa@hotmail.com>
2021-07-27 02:17:55 -0300

    chore(routes): use ListIncidentController

--- src/routes/incidents.routes.ts
+++ src/routes/incidents.routes.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express'
 
-import { CreateIncidentController, ListIncidentsController } from '@controllers'
+import {
+  CreateIncidentController,
+  ListIncidentsController,
+  ListIncidentController
+} from '@controllers'
 
 const createIncidentController = new CreateIncidentController()
 const listIncidentsController = new ListIncidentsController()
+const listIncidentController = new ListIncidentController()
 
 const routes = Router()
 
 routes.post('/', createIncidentController.handle)
 routes.get('/', listIncidentsController.handle)
+routes.get('/:id', listIncidentController.handle)
 
 export { routes }
