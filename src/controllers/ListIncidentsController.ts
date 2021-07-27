76e303fccf88736dd4e5ba2b68fb30654c83f3d3
HallexCosta <hallex.costa@hotmail.com>
2021-07-27 16:36:28 -0300

    chore(controllers): pass ong_id to the incident list service

--- src/controllers/ListIncidentsController.ts
+++ src/controllers/ListIncidentsController.ts
@@ -3,9 +3,13 @@ import { ListIncidentsService } from '@services'
 
 export class ListIncidentsController {
   public async handle(request: Request, response: Response): Promise<Response> {
+    const ong_id = request.query.ong_id as string
+
     const service = new ListIncidentsService()
 
-    const incidents = await service.execute()
+    const incidents = await service.execute({
+      ong_id
+    })
 
     return response.json(incidents)
   }
