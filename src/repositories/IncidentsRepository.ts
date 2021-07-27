a55f8116333450c384def7db14517d9ffbeda615
HallexCosta <hallex.costa@hotmail.com>
2021-07-27 02:17:31 -0300

    chore(repositories): add func findById in IncidentsRepository

--- src/repositories/IncidentsRepository.ts
+++ src/repositories/IncidentsRepository.ts
@@ -3,4 +3,10 @@ import { EntityRepository, Repository } from 'typeorm'
 import { Incident } from '@entities'
 
 @EntityRepository(Incident)
-export class IncidentsRepository extends Repository<Incident> {}
+export class IncidentsRepository extends Repository<Incident> {
+  async findById(id: string): Promise<Incident> {
+    return await this.findOne({
+      id
+    })
+  }
+}
