dde795b0482379f7b4bc86d1645e1b3741eb3553
HallexCosta <hallex.costa@hotmail.com>
2021-07-27 03:16:43 -0300

    feat(controllers): add UpdateIncidentController

---
+++ src/controllers/UpdateIncidentController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+
+import { UpdateIncidentService } from '@services'
+
+export class UpdateIncidentController {
+  public async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params
+    const { name, coast, description } = request.body
+
+    const service = new UpdateIncidentService()
+
+    const incident = await service.execute({ id, name, coast, description })
+
+    return response.json(incident)
+  }
+}
