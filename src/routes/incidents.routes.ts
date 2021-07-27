4719827364d350eb97cf6e0c48c7452fdfe77f42
HallexCosta <hallex.costa@hotmail.com>
2021-07-27 03:17:38 -0300

    chore(routes): use UpdateIncidentController

--- src/routes/incidents.routes.ts
+++ src/routes/incidents.routes.ts
@@ -3,17 +3,20 @@ import { Router } from 'express'
 import {
   CreateIncidentController,
   ListIncidentsController,
-  ListIncidentController
+  ListIncidentController,
+  UpdateIncidentController
 } from '@controllers'
 
 const createIncidentController = new CreateIncidentController()
 const listIncidentsController = new ListIncidentsController()
 const listIncidentController = new ListIncidentController()
+const updateIncidentController = new UpdateIncidentController()
 
 const routes = Router()
 
 routes.post('/', createIncidentController.handle)
 routes.get('/', listIncidentsController.handle)
 routes.get('/:id', listIncidentController.handle)
+routes.patch('/:id', updateIncidentController.handle)
 
 export { routes }
