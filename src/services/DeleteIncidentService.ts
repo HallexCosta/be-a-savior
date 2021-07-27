52e91ea3ba2984fb18d879c9d9f434e569dcce0d
HallexCosta <hallex.costa@hotmail.com>
2021-07-27 18:45:30 -0300

    feat(services): add DeleteIncidentService

---
+++ src/services/DeleteIncidentService.ts
@@ -0,0 +1,36 @@
+import { DeleteResult, getCustomRepository } from 'typeorm'
+
+import { Incident } from '@entities'
+import { IncidentsRepository } from '@repositories'
+
+type DeleteIncidentDTO = {
+  id: string
+}
+
+export class DeleteIncidentService {
+  public async execute({ id }: DeleteIncidentDTO): Promise<Incident> {
+    const repository = getCustomRepository(IncidentsRepository)
+
+    const incident = await repository.findById(id)
+
+    this.checkIncidentExists(incident)
+
+    const result = await repository.deleteById(id)
+
+    this.checkIncidentDeleteWithSuccess(result)
+
+    return incident
+  }
+
+  private checkIncidentExists(incident: Incident): void {
+    if (!incident) {
+      throw new Error('Incident not exists')
+    }
+  }
+
+  private checkIncidentDeleteWithSuccess(result: DeleteResult): void {
+    if (result.affected <= 0) {
+      throw new Error('Unable to delete the incident')
+    }
+  }
+}
