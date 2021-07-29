09230c9abbaa9abbab88299ba04f971b690174ad
HallexCosta <hallex.costa@hotmail.com>
2021-07-29 02:23:25 -0300

    feat(middlewares): add ensureDonor

---
+++ src/middlewares/ensureDonor.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express'
+import { getCustomRepository } from 'typeorm'
+
+import { DonorsRepository } from '@repositories'
+
+export async function ensureDonor(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<Response | void> {
+  const { donor_id } = request
+
+  const repository = getCustomRepository(DonorsRepository)
+
+  const donor = await repository.findById(donor_id)
+
+  if (donor) {
+    return next()
+  }
+
+  return response.status(401).json({
+    error: 'Unauthorized: Ong not found'
+  })
+}
