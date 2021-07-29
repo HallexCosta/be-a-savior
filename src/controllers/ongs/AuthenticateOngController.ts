89e3ea993d115fde4145b6c8f43b8ca3533e9a92
HallexCosta <hallex.costa@hotmail.com>
2021-07-29 02:05:37 -0300

    feat(controllers): add AuthenticateOngController

---
+++ src/controllers/ongs/AuthenticateOngController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express'
+import { AuthenticateOngService } from '@services'
+
+export class AuthenticateOngController {
+  public async handle(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body
+
+    const service = new AuthenticateOngService()
+
+    const token = await service.execute({
+      email,
+      password
+    })
+
+    return response.json({
+      token
+    })
+  }
+}
