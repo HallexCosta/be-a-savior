e7011ca0d9c6a7336b0aedbe0d968f57affd89e7
HallexCosta <hallex.costa@hotmail.com>
2021-08-10 01:37:30 -0300

    chore(routes): export donationsRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
 export { routes as donorsRoutes } from './donors.routes'
 export { routes as ongsRoutes } from './ongs.routes'
 export { routes as incidentsRoutes } from './incidents.routes'
+export { routes as donationsRoutes } from './donations.routes'
