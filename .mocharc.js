81b5ca02f13d5166c1200eeb085052ac251f1da7
HallexCosta <hallex.costa@hotmail.com>
2021-08-11 17:28:19 -0300

    chore(.mocharc): remove unused file propperty

--- .mocharc.js
+++ .mocharc.js
@@ -5,7 +5,6 @@ module.exports = {
   extension: ['ts'],
   package: './package.json',
   reporter: 'landing',
-  file: [],
   require: ['dotenv/config', 'ts-node/register', 'tsconfig-paths/register'],
   timeout: 0,
   spec: ['__tests__/unit/**/*.spec.ts', '__tests__/integration/**/*.spec.ts'],
