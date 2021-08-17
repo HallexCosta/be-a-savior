31212571316ecc165206b96691c2dea34e8fea26
HallexCosta <hallex.costa@hotmail.com>
2021-08-17 13:33:19 -0300

    chore(ormconfig): set ormconfig by envrionment TS or Test

--- ormconfig.js
+++ ormconfig.js
@@ -1,17 +1,37 @@
-const database = {
-  production: 'src/database/database.sqlite',
-  test: 'src/database/database-test.sqlite'
+function checkEnvironmentFromDatabase(path) {
+  const paths = path.split('/')
+  const file = paths.pop()
+  const filename = file.split('-')
+  const envrionment = filename.pop()
+
+  return envrionment === process.env.NODE_ENV
 }
 
-const databasePath = database[process.env.DATABASE] ?? database.production
-
-module.exports = {
-  type: 'sqlite',
-  database: databasePath,
-  migrations: ['src/database/migrations/*.ts'],
-  entities: ['src/entities/*.ts'],
-  cli: {
-    migrationsDir: 'src/database/migrations',
-    entitiesDir: 'src/entities'
+const configs = [
+  {
+    type: 'sqlite',
+    database: 'src/database/database.sqlite',
+    migrations: ['src/database/migrations/*.ts'],
+    entities: ['src/entities/*.ts'],
+    cli: {
+      migrationsDir: 'src/database/migrations',
+      entitiesDir: 'src/entities'
+    }
+  },
+  {
+    type: 'sqlite',
+    database: 'src/database/database.sqlite-test',
+    migrations: ['src/database/migrations/*.ts'],
+    entities: ['src/entities/*.ts'],
+    cli: {
+      migrationsDir: 'src/database/migrations',
+      entitiesDir: 'src/entities'
+    }
   }
-}
+]
+
+const config = configs.find(({ database: path }) =>
+  checkEnvironmentFromDatabase(path)
+)
+
+module.exports = config
