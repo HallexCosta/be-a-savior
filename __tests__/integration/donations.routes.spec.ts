2c2246ceb6e6cc7c0ca664b842a66e0bde608856
HallexCosta <hallex.costa@hotmail.com>
2021-08-30 23:05:16 -0300

    test(integration): all endpoints to running in integration scope global

--- __tests__/integration/donations.routes.spec.ts
+++ __tests__/integration/donations.routes.spec.ts
@@ -20,40 +20,42 @@ import {
 
 use(dirty)
 
-describe('Donation Routes', () => {
-  let agent: SuperTest<Test>
-  let ong: Ong
-  let donor: Donor
-  let incident: Incident
-  let token: string
+export const donations = () => {
+  describe('Donation Routes', () => {
+    let agent: SuperTest<Test>
+    let ong: Ong
+    let donor: Donor
+    let incident: Incident
+    let token: string
 
-  before(async () => {
-    await createTestingConnection()
-    agent = createAgent(app)
-    ong = await createFakeOng(agent)
-    donor = await createFakeDonor(agent)
-    incident = await createFakeIncident(agent, {
-      ong_id: ong.id,
-      token: await loginWithFakeOng(agent)
+    before(async () => {
+      await createTestingConnection()
+      agent = createAgent(app)
+      ong = await createFakeOng(agent)
+      donor = await createFakeDonor(agent)
+      incident = await createFakeIncident(agent, {
+        ong_id: ong.id,
+        token: await loginWithFakeOng(agent)
+      })
+      token = await loginWithFakeDonor(agent)
     })
-    token = await loginWithFakeDonor(agent)
+
+    it('Should be create new Donation POST (/donations)', async () => {
+      const body = {
+        amount: 1788,
+        incident_id: incident.id,
+        donor_id: donor.id
+      }
+
+      const response = await agent
+        .post('/donations')
+        .send(body)
+        .set('Authorization', `bearer ${token}`)
+
+      const expected = response.body
+
+      expect(expected).to.not.be.undefined()
+      expect(expected).to.have.property('id')
+    })
   })
-
-  it('Should be create new Donation POST (/donations)', async () => {
-    const body = {
-      amount: 1788,
-      incident_id: incident.id,
-      donor_id: donor.id
-    }
-
-    const response = await agent
-      .post('/donations')
-      .send(body)
-      .set('Authorization', `bearer ${token}`)
-
-    const expected = response.body
-
-    expect(expected).to.not.be.undefined()
-    expect(expected).to.have.property('id')
-  })
-})
+}
--- __tests__/integration/ongs.routes.spec.ts
+++ __tests__/integration/ongs.routes.spec.ts
@@ -8,66 +8,68 @@ import { createAgent, createTestingConnection } from './fakes/stubs'
 
 use(dirty)
 
-describe('Ongs Routes', () => {
-  let id: string
-  let agent: SuperTest<Test>
+export const ongs = () => {
+  describe('Ongs Routes', () => {
+    let id: string
+    let agent: SuperTest<Test>
 
-  before(async () => {
-    await createTestingConnection()
-    agent = createAgent(app)
+    before(async () => {
+      await createTestingConnection()
+      agent = createAgent(app)
+    })
+
+    it('Should be able to create new Ong POST (/ongs)', async () => {
+      const body = {
+        name: 'Some a name',
+        email: 'some@hotmail.com',
+        password: 'some123',
+        phone: '(99) 99999-9999'
+      }
+
+      const response = await agent.post('/ongs').send(body)
+
+      const expected = response.body
+
+      id = expected.id
+
+      expect(expected).to.not.be.undefined()
+      expect(expected).to.have.property('id')
+      expect(expected).to.have.property('created_at')
+      expect(expected).to.have.property('updated_at')
+    })
+
+    it('Should be able authenticate ong POST (/ongs/login)', async () => {
+      const body = {
+        email: 'some@hotmail.com',
+        password: 'some123'
+      }
+
+      const response = await agent.post('/ongs/login').send(body)
+      const expected = response.body
+
+      expect(expected).to.have.property('token')
+      expect(expected).to.not.be.undefined()
+    })
+
+    it('Should be able list Ongs GET (/ongs)', async () => {
+      const response = await agent.get('/ongs')
+
+      const expected = response.body
+
+      expect(expected[0]).to.have.property('id')
+      expect(expected[0]).to.have.property('created_at')
+      expect(expected[0]).to.have.property('updated_at')
+    })
+
+    it('Should be able list one Ong by Id GET (/ongs/:id)', async () => {
+      const response = await agent.get(`/ongs/${id}`)
+
+      const expected = response.body
+
+      expect(expected).to.have.property('id')
+      expect(expected.email).to.be.equal('some@hotmail.com')
+      expect(expected).to.have.property('created_at')
+      expect(expected).to.have.property('updated_at')
+    })
   })
-
-  it('Should be able to create new Ong POST (/ongs)', async () => {
-    const body = {
-      name: 'Some a name',
-      email: 'some@hotmail.com',
-      password: 'some123',
-      phone: '(99) 99999-9999'
-    }
-
-    const response = await agent.post('/ongs').send(body)
-
-    const expected = response.body
-
-    id = expected.id
-
-    expect(expected).to.not.be.undefined()
-    expect(expected).to.have.property('id')
-    expect(expected).to.have.property('created_at')
-    expect(expected).to.have.property('updated_at')
-  })
-
-  it('Should be able authenticate ong POST (/ongs/login)', async () => {
-    const body = {
-      email: 'some@hotmail.com',
-      password: 'some123'
-    }
-
-    const response = await agent.post('/ongs/login').send(body)
-    const expected = response.body
-
-    expect(expected).to.have.property('token')
-    expect(expected).to.not.be.undefined()
-  })
-
-  it('Should be able list Ongs GET (/ongs)', async () => {
-    const response = await agent.get('/ongs')
-
-    const expected = response.body
-
-    expect(expected[0]).to.have.property('id')
-    expect(expected[0]).to.have.property('created_at')
-    expect(expected[0]).to.have.property('updated_at')
-  })
-
-  it('Should be able list one Ong by Id GET (/ongs/:id)', async () => {
-    const response = await agent.get(`/ongs/${id}`)
-
-    const expected = response.body
-
-    expect(expected).to.have.property('id')
-    expect(expected.email).to.be.equal('some@hotmail.com')
-    expect(expected).to.have.property('created_at')
-    expect(expected).to.have.property('updated_at')
-  })
-})
+}
