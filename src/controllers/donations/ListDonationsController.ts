b10f504f323066c92b55a45fc4f9a6f8a0cd154e
HallexCosta <hallex.costa@hotmail.com>
2021-09-02 00:05:15 -0300

    chore(controllers): catch ong id from query params and pass to service

--- src/controllers/donations/ListDonationsController.ts
+++ src/controllers/donations/ListDonationsController.ts
@@ -3,10 +3,14 @@ import { Request, Response } from 'express'
 import { ListDonationsService } from '@services/donations/ListDonationsService'
 
 export class ListDonationsController {
-  public async handle(_: Request, response: Response): Promise<Response> {
+  public async handle(request: Request, response: Response): Promise<Response> {
+    const ongId = request.query.ong_id as string
+
     const service = new ListDonationsService()
 
-    const incident = await service.execute()
+    const incident = await service.execute({
+      ongId
+    })
 
     return response.json(incident)
   }
