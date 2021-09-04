1277a1e3f18f04713369ad6de87466f5a5f2c79a
HallexCosta <hallex.costa@hotmail.com>
2021-09-03 23:59:56 -0300

    chore(controllers): add parse query params to work together filters

--- src/controllers/incidents/ListIncidentsController.ts
+++ src/controllers/incidents/ListIncidentsController.ts
@@ -2,16 +2,35 @@ import { Request, Response } from 'express'
 
 import { ListIncidentsService } from '@services/incidents/ListIncidentsService'
 
+type QueryParams = {
+  ongId?: string
+  donated: boolean
+}
+
 export class ListIncidentsController {
   public async handle(request: Request, response: Response): Promise<Response> {
-    const ong_id = request.query.ong_id as string
+    const { ongId, donated } = this.queryParams(request.query)
 
     const service = new ListIncidentsService()
 
     const incidents = await service.execute({
-      ong_id
+      ongId,
+      donated
     })
 
     return response.json(incidents)
+  }
+
+  private queryParams(params: any): QueryParams {
+    const queryParams = {} as QueryParams
+
+    queryParams.donated = params.donated === 'true'
+    queryParams.ongId = params.ong_id
+
+    if (!params.donated) {
+      queryParams.donated = undefined
+    }
+
+    return queryParams
   }
 }
