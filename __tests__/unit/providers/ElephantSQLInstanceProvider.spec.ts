477789f5f6f037f81fd107e520cb14cb9f6d2cc6
HallexCosta <hallex.costa@hotmail.com>
2021-10-15 01:17:56 -0300

    test(unit): now test should follow type Instance

--- __tests__/unit/providers/ElephantSQLInstanceProvider.spec.ts
+++ __tests__/unit/providers/ElephantSQLInstanceProvider.spec.ts
@@ -16,9 +16,10 @@ const mockInstances = new Map<string, Instance>([
       id: 239286,
       name: 'be-a-savior-test',
       plan: 'turtle',
+      url: 'postgres://gxzxslmv:B3FamoI_FTyur7w9wrbMiP04BK-zwMRW@fanny.db.elephantsql.com/gxzxslmv',
       region: 'amazon-web-services::us-east-1',
-      tags: [],
-      providerid: 'd2b69142-4088-433c-897e-19225af6c7c2'
+      providerid: 'd2b69142-4088-433c-897e-19225af6c7c2',
+      apikey: '1135e7d8-087b-4451-828a-b3fcf3161669'
     },
   ],
   [
@@ -26,9 +27,10 @@ const mockInstances = new Map<string, Instance>([
       id: 239154,
       name: 'be-a-savior-development',
       plan: 'turtle',
+      url: 'postgres://gxzxslmv:B3FamoI_FTyur7w9wrbMiP04BK-zwMRW@fanny.db.elephantsql.com/gxzxslmv',
       region: 'amazon-web-services::us-east-1',
-      tags: [],
-      providerid: '9ec59457-c81c-437b-b936-5fc84a60de7c'
+      providerid: '9ec59457-c81c-437b-b936-5fc84a60de7c',
+      apikey: '1135e7d8-087b-4451-828a-b3fcf3161669'
     },
   ],
   [
@@ -36,9 +38,10 @@ const mockInstances = new Map<string, Instance>([
       id: 239154,
       name: 'be-a-savior-production',
       plan: 'turtle',
+      url: 'postgres://gxzxslmv:B3FamoI_FTyur7w9wrbMiP04BK-zwMRW@fanny.db.elephantsql.com/gxzxslmv',
       region: 'amazon-web-services::us-east-1',
-      tags: [],
-      providerid: '9ec59457-c81c-437b-b936-5fc84a60de7c'
+      providerid: '9ec59457-c81c-437b-b936-5fc84a60de7c',
+      apikey: '1135e7d8-087b-4451-828a-b3fcf3161669'
     },
   ]
 ])
@@ -46,56 +49,19 @@ const mockInstances = new Map<string, Instance>([
 
 describe('#ElephantSQLInstanceProvider', () => {
   let sandbox: sinon.SinonSandbox
+  let defaultElephantParam = null
 
-  describe('#construct', () => {
+  describe('#constructor', () => {
     beforeEach(() => sandbox = sinon.createSandbox())
     afterEach(() => sandbox.restore())
 
     it('should create new instance of ElephantSQLInstanceProvider', () => {
-      expect(new ElephantSQLInstanceProvider()).to.be.instanceOf(ElephantSQLInstanceProvider)
-    })
-  })
+      const apikey = 'this-is-api-key'
+      const apiInterceptorsRequestUseSpy = sandbox.spy(api.interceptors.request, 'use')
+      const elephantInstanceProvider = new ElephantSQLInstanceProvider(apikey)
 
-  describe('#load', () => {
-    beforeEach(() => sandbox = sinon.createSandbox())
-    afterEach(() => sandbox.restore())
-
-    it('should load map from instances with all instances in remote', async () => {
-      const elephantInstanceProvider = new ElephantSQLInstanceProvider()
-
-      const mockInstancesRemote = mockInstances.values()
-      sandbox.stub(elephantInstanceProvider, 'load').callsFake(async function() {
-        for (const instance of mockInstancesRemote) {
-          this.instances.set(instance.name, instance)
-        }
-      })
-      await elephantInstanceProvider.load()
-
-      expect(elephantInstanceProvider.instances.size).to.be.greaterThanOrEqual(3)
-    })
-  })
-
-
-  describe('#findInMemory', () => {
-    beforeEach(() => sandbox = sinon.createSandbox())
-    afterEach(() => sandbox.restore())
-
-    it('should find instance in memory', () => {
-      const elephantInstanceProvider = new ElephantSQLInstanceProvider()
-      sandbox.stub(elephantInstanceProvider, 'instances').value(mockInstances)
-
-      const expectedInstance = elephantInstanceProvider.findInMemory('be-a-savior-test')
-
-      expect(expectedInstance).to.be.equal(elephantInstanceProvider.instances.get('be-a-savior-test'))
-    })
-
-    it('should return undefined if not found instance in memory', () => {
-      const elephantInstanceProvider = new ElephantSQLInstanceProvider()
-      sandbox.stub(elephantInstanceProvider, 'instances').value(mockInstances)
-
-      const expectedInstance = elephantInstanceProvider.findInMemory('return-undefined-instance')
-
-      expect(expectedInstance).to.be.undefined()
+      expect(elephantInstanceProvider).to.be.instanceOf(ElephantSQLInstanceProvider)
+      expect(apiInterceptorsRequestUseSpy.calledOnce).to.be.true()
     })
   })
 
@@ -104,7 +70,7 @@ describe('#ElephantSQLInstanceProvider', () => {
     afterEach(() => sandbox.restore())
 
     it('should find and return instance by name in remote', async () => {
-      const elephantInstanceProvider = new ElephantSQLInstanceProvider()
+      const elephantInstanceProvider = new ElephantSQLInstanceProvider(defaultElephantParam)
 
       const instanceName = 'be-a-savior-test'
       const instances = [...mockInstances.values()]
@@ -116,7 +82,7 @@ describe('#ElephantSQLInstanceProvider', () => {
     })
 
     it('should return undefined if not found instance by name in remote', async () => {
-      const elephantInstanceProvider = new ElephantSQLInstanceProvider()
+      const elephantInstanceProvider = new ElephantSQLInstanceProvider(defaultElephantParam)
       const apiGetStub = sandbox.stub(api, 'get').resolves({
         data: []
       })
@@ -132,22 +98,8 @@ describe('#ElephantSQLInstanceProvider', () => {
     beforeEach(() => sandbox = sinon.createSandbox())
     afterEach(() => sandbox.restore())
 
-    it('should return true if instance already exists in memory', async () => {
-      const elephantInstanceProvider = new ElephantSQLInstanceProvider()
-
-      sandbox.stub(elephantInstanceProvider, 'findInRemote').resolves(undefined)
-      const findInMemoryStub = sandbox.stub(elephantInstanceProvider, 'findInMemory').returns([...mockInstances.values()][0])
-
-      const instanceName = 'be-a-savior-test'
-      const alreadyExists = await elephantInstanceProvider.verifyInstanceExists(instanceName)
-
-      expect(findInMemoryStub.calledOnce).to.be.true()
-      expect(findInMemoryStub.calledWith(instanceName)).to.be.true()
-      expect(alreadyExists).to.be.true()
-    })
-
     it('should return true if instance already exists in remote', async () => {
-      const elephantInstanceProvider = new ElephantSQLInstanceProvider()
+      const elephantInstanceProvider = new ElephantSQLInstanceProvider(defaultElephantParam)
 
       const findInRemoteStub = sandbox.stub(elephantInstanceProvider, 'findInRemote').resolves([...mockInstances.values()][0])
 
@@ -159,14 +111,12 @@ describe('#ElephantSQLInstanceProvider', () => {
     })
 
     it('should return false if not found instance in memory or remote', async () => {
-      const elephantInstanceProvider = new ElephantSQLInstanceProvider()
+      const elephantInstanceProvider = new ElephantSQLInstanceProvider(defaultElephantParam)
 
-      const findInMemoryStub = sandbox.stub(elephantInstanceProvider, 'findInMemory').returns(undefined)
       const findInRemoteStub = sandbox.stub(elephantInstanceProvider, 'findInRemote').resolves(undefined)
 
       const alreadyExists = await elephantInstanceProvider.verifyInstanceExists('be-a-savior-test')
 
-      expect(findInMemoryStub.calledOnce).to.be.true()
       expect(findInRemoteStub.calledOnce).to.be.true()
       expect(alreadyExists).to.be.false()
     })
@@ -176,21 +126,8 @@ describe('#ElephantSQLInstanceProvider', () => {
     beforeEach(() => sandbox = sinon.createSandbox())
     afterEach(() => sandbox.restore())
 
-    it('should find instance id by name in memory', async () => {
-      const elephantInstanceProvider = new ElephantSQLInstanceProvider()
-
-      const mockInstance = [...mockInstances.values()][0]
-
-      sandbox.stub(elephantInstanceProvider, 'findInRemote').resolves(null)
-      const findInMemoryStub = sandbox.stub(elephantInstanceProvider, 'findInMemory').returns(mockInstance)
-      const instanceId = await elephantInstanceProvider.findInstanceId(mockInstance.name)
-
-      expect(findInMemoryStub.calledOnce).to.be.true()
-      expect(instanceId).to.be.equal(mockInstance.id)
-    })
-
     it('should find instance id by name in remote', async () => {
-      const elephantInstanceProvider = new ElephantSQLInstanceProvider()
+      const elephantInstanceProvider = new ElephantSQLInstanceProvider(defaultElephantParam)
 
       const mockInstance = [...mockInstances.values()][0]
       const findInRemoteStub = sandbox.stub(elephantInstanceProvider, 'findInRemote').resolves(mockInstance)
@@ -202,10 +139,9 @@ describe('#ElephantSQLInstanceProvider', () => {
     })
 
     it('should return null if not found instance id', async () => {
-      const elephantInstanceProvider = new ElephantSQLInstanceProvider()
+      const elephantInstanceProvider = new ElephantSQLInstanceProvider(defaultElephantParam)
 
       const mockInstance = [...mockInstances.values()][0]
-      sandbox.stub(elephantInstanceProvider, 'findInMemory').returns(null)
       sandbox.stub(elephantInstanceProvider, 'findInRemote').returns(null)
       const instanceIdNotFound = await elephantInstanceProvider.findInstanceId(mockInstance.name)
 
@@ -213,72 +149,95 @@ describe('#ElephantSQLInstanceProvider', () => {
     })
   })
 
+  describe('#listInstances', () => {
+    beforeEach(() => sandbox = sinon.createSandbox())
+    afterEach(() => sandbox.restore())
+
+    it('should list all instances', async () => {
+      const elephantInstanceProvider = new ElephantSQLInstanceProvider(defaultElephantParam)
+
+      const mockApiGetResponse = [...mockInstances.values()].map(({ id, name, plan, region, providerid }) => ({
+        id, name, plan, region, tags: [], providerid
+      }))
+
+      const apiGetStub = sandbox.stub(api, 'get')
+
+      apiGetStub.onCall(0).resolves({
+        data: mockApiGetResponse
+      })
+      apiGetStub.onCall(1).resolves({ data: [...mockInstances.values()][0] })
+      apiGetStub.onCall(2).resolves({ data: [...mockInstances.values()][1] })
+      apiGetStub.onCall(3).resolves({ data: [...mockInstances.values()][2] })
+
+      const expectedInstances = await elephantInstanceProvider.listInstances()
+
+      expect(expectedInstances).to.deep.equalInAnyOrder([...mockInstances.values()])
+    })
+  })
+
+
   describe('#createInstance', () => {
     beforeEach(() => sandbox = sinon.createSandbox())
     afterEach(() => sandbox.restore())
 
-    it('create new instance remote in elephant', async () => {
-      const elephantInstanceProvider = new ElephantSQLInstanceProvider()
-      const instance = {
+    it('create new instance in elephant service', async () => {
+      const mockInstance = {
         name: 'this-is-a-new-instance50',
         plan: 'turtle',
         region: 'amazon-web-services::us-east-1',
       }
+      const mockApiPostData = {
+        id: 239644,
+        message: 'Instance created',
+        apikey: 'd6ef6f30-5107-4edb-8da7-6babc2c52742',
+        url: 'postgres://zsqkwtxb:UiT8NZVLusnZ3yuYkL8jrURNg5OGTvIV@fanny.db.elephantsql.com/zsqkwtxb'
+      }
+      const mockApiGetData = {
+        id: 239154,
+        name: "this-is-a-new-instance50",
+        plan: "turtle",
+        region: "amazon-web-services::us-east-1",
+        tags: [],
+        providerid: "9ec59457-c81c-437b-b936-5fc84a60de7c",
+        url: "postgres://gxzxslmv:B3FamoI_FTyur7w9wrbMiP04BK-zwMRW@fanny.db.elephantsql.com/gxzxslmv",
+        apikey: "1135e7d8-087b-4451-828a-b3fcf3161669",
+        ready: true
+      }
 
-      sandbox.stub(api, 'get').resolves({
-        data: []
-      })
       const apiPostStub = sandbox.stub(api, 'post').resolves({
-        data: {
-          id: 239644,
-          message: 'Instance created',
-          apikey: 'd6ef6f30-5107-4edb-8da7-6babc2c52742',
-          url: 'postgres://zsqkwtxb:UiT8NZVLusnZ3yuYkL8jrURNg5OGTvIV@fanny.db.elephantsql.com/zsqkwtxb'
-        }
+        data: mockApiPostData
+      })
+      const apiGetStub = sandbox.stub(api, 'get').resolves({
+        data: mockApiGetData
       })
 
-      const newInstanceCreated = await elephantInstanceProvider.createInstance(instance)
+      const elephantInstanceProvider = new ElephantSQLInstanceProvider(defaultElephantParam)
+      sandbox.stub(elephantInstanceProvider, 'verifyInstanceExists').resolves(undefined)
+
+      const newInstanceCreated = await elephantInstanceProvider.createInstance(mockInstance)
 
       expect(apiPostStub.calledOnce).to.be.true()
-      expect(newInstanceCreated).to.have.property('id')
-      expect(newInstanceCreated).to.have.property('message')
-      expect(newInstanceCreated).to.have.property('apikey')
-      expect(newInstanceCreated).to.have.property('url')
-      expect(newInstanceCreated.message).to.be.equal('Instance created')
-    })
-
-    it('should thrown an error if already exists an instance with same name in memory', async () => {
-      const instance = {
-        name: 'be-a-savior-test',
-        plan: 'turtle',
-        region: 'amazon-web-services::us-east-1',
-      }
-
-      const elephantInstanceProvider = new ElephantSQLInstanceProvider()
-
-      sandbox.stub(elephantInstanceProvider, 'instances').value(mockInstances)
-      sandbox.stub(api, 'get').resolves({ data: [] })
-
-      const toThrow = async () => {
-        await elephantInstanceProvider.createInstance(instance)
-      }
-
-      await expect(toThrow()).to.be.rejectedWith(Error)
+      expect(apiGetStub.calledOnce).to.be.true()
+      expect(newInstanceCreated).to.deep.equal({
+        id: 239154,
+        name: "this-is-a-new-instance50",
+        plan: "turtle",
+        region: "amazon-web-services::us-east-1",
+        providerid: "9ec59457-c81c-437b-b936-5fc84a60de7c",
+        url: "postgres://gxzxslmv:B3FamoI_FTyur7w9wrbMiP04BK-zwMRW@fanny.db.elephantsql.com/gxzxslmv",
+        apikey: "1135e7d8-087b-4451-828a-b3fcf3161669",
+      })
     })
 
     it('should thrown an error if already exists an instance with same name in remote', async () => {
-      const elephantInstanceProvider = new ElephantSQLInstanceProvider()
+      const elephantInstanceProvider = new ElephantSQLInstanceProvider(defaultElephantParam)
       const instance = {
         name: 'be-a-savior-test',
         plan: 'turtle',
         region: 'amazon-web-services::us-east-1',
       }
 
-      const mockInstance = [...mockInstances.values()].find(({ name }) => name === instance.name)
-
-      sandbox.stub(api, 'get').resolves({
-        data: [mockInstance]
-      })
+      sandbox.stub(elephantInstanceProvider, 'verifyInstanceExists').resolves(true)
 
       const toThrow = async () => {
         await elephantInstanceProvider.createInstance(instance)
@@ -293,9 +252,8 @@ describe('#ElephantSQLInstanceProvider', () => {
     afterEach(() => sandbox.restore())
 
     it('should delete an instance remote by name', async () => {
-      const elephantInstanceProvider = new ElephantSQLInstanceProvider()
+      const elephantInstanceProvider = new ElephantSQLInstanceProvider(defaultElephantParam)
 
-      sandbox.stub(elephantInstanceProvider, 'instances').value(mockInstances)
       sandbox.stub(elephantInstanceProvider, 'findInstanceId').resolves(123456)
       const apiDeleteStub = sandbox.stub(api, 'delete')
 
@@ -303,11 +261,10 @@ describe('#ElephantSQLInstanceProvider', () => {
 
       expect(deleted).to.be.true()
       expect(apiDeleteStub.calledOnce).to.be.true()
-      expect(elephantInstanceProvider.instances.get('be-a-savior-test')).to.be.undefined()
     })
 
     it('should return false if instance already deleted', async () => {
-      const elephantInstanceProvider = new ElephantSQLInstanceProvider()
+      const elephantInstanceProvider = new ElephantSQLInstanceProvider(defaultElephantParam)
 
       sandbox.stub(elephantInstanceProvider, 'findInstanceId').resolves(null)
       const apiDeleteStub = sandbox.stub(api, 'delete').rejects('Request failed with status code 404')
