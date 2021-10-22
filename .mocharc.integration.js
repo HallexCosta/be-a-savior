6fd8a3b0a444b2b59662cc7d09ad9decb282023a
HallexCosta <hallex.costa@hotmail.com>
2021-10-22 17:04:15 -0300

    chore(mocharc): import setup in mocha integration cofigs

--- .mocharc.integration.js
+++ .mocharc.integration.js
@@ -2,6 +2,7 @@ const config = require('./.mocharc.global')
 
 module.exports = {
   ...config,
+  file: ['__tests__/setup.ts'],
   spec: ['__tests__/integration/**/*.spec.ts'],
   exclude: ['__tests__/integration/isolate/*.spec.ts']
 }
