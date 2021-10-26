643a140d45fb68f7a93e3968511890bc0afb560c
HallexCosta <hallex.costa@hotmail.com>
2021-10-25 22:32:20 -0300

    test(integration): adjust donations and incidents test integration

--- __tests__/integration/donations.routes.spec.ts
+++ __tests__/integration/donations.routes.spec.ts
@@ -9,6 +9,7 @@ import { Incident } from '@entities/Incident'
 import { Ong } from '@entities/Ong'
 
 import {
+  mock,
   createTestingConnection,
   createFakeIncident,
   createAgent,
@@ -16,93 +17,89 @@ import {
   loginWithFakeDonor,
   createFakeDonor,
   loginWithFakeOng
-} from './fakes/stubs'
+} from './fakes/mocks'
 
 use(dirty)
 
-export function donations(): void {
-  describe('Donation Routes', () => {
-    let agent: SuperTest<Test>
-    let ong: Ong
-    let donor: Donor
-    let incident: Incident
-    let token: string
+describe('Donation Routes', () => {
+  let agent: SuperTest<Test>
+  let ong: Ong
+  let donor: Donor
+  let incident: Incident
+  let donorToken: string
 
-    before(async () => {
-      await createTestingConnection()
-      agent = createAgent(app)
-      ong = await createFakeOng(agent)
-      donor = await createFakeDonor(agent)
-      incident = await createFakeIncident(agent, {
-        ong_id: ong.id,
-        ongToken: await loginWithFakeOng(agent)
-      })
-      token = await loginWithFakeDonor(agent)
+  before(async () => {
+    await createTestingConnection()
+    agent = createAgent(app)
+    ong = await createFakeOng(agent)
+    donor = await createFakeDonor(agent)
+    incident = await createFakeIncident(agent, {
+      ong_id: ong.id,
+      ongToken: await loginWithFakeOng(agent)
     })
-
-    it('Should be create new Donation POST (/donations)', async () => {
-      const body = {
-        incident_id: incident.id,
-        donor_id: donor.id
-      }
-
-      const response = await agent
-        .post('/donations')
-        .send(body)
-        .set('Authorization', `bearer ${token}`)
-
-      const expected = response.body
-
-      expect(expected).to.not.be.undefined()
-      expect(expected).to.have.property('id')
-    })
-
-    it('Should be thrown error if try donate in same incident POST (/donations)', async () => {
-      const body = {
-        incident_id: incident.id,
-        donor_id: donor.id
-      }
-
-      const response = await agent
-        .post('/donations')
-        .send(body)
-        .set('Authorization', `bearer ${token}`)
-
-      const expected = response.body
-
-      expect(expected).to.have.property('message')
-    })
-
-    it('Should be to list all donations GET (/donations)', async () => {
-      const response = await agent.get('/donations').send()
-
-      const expected = response.body
-
-      expect(expected).to.not.be.undefined()
-      expect(expected[0]).to.have.property('id')
-      expect(expected[0]).to.have.property('incident_id')
-      expect(expected[0]).to.have.property('donor_id')
-    })
-
-    it('Should be to return empty list donations with fake ong_id GET (/donations?ong_id=)', async () => {
-      const response = await agent.get('/donations?ong_id=FAKE ONG ID').send()
-
-      const expected = response.body
-
-      expect(expected).to.not.be.undefined()
-      expect(expected).to.have.lengthOf(0)
-    })
-
-    it('Should be to list donations by ong_id GET (/donations?ong_id=)', async () => {
-      const response = await agent.get(`/donations?ong_id=${ong.id}`).send()
-
-      const expected = response.body
-
-      expect(expected).to.not.be.undefined()
-      expect(expected[0]).to.have.property('id')
-      expect(expected[0]).to.have.property('incident_id')
-      expect(expected[0]).to.have.property('donor_id')
-      expect(expected[0]).to.have.property('ong_id')
-    })
+    donorToken = await loginWithFakeDonor(agent)
   })
-}
+
+  it('Should be create new Donation POST (/donations)', async () => {
+    const body = {
+      incident_id: incident.id
+    }
+
+    const response = await agent
+      .post('/donations')
+      .send(body)
+      .set('Authorization', `bearer ${donorToken}`)
+
+    const expected = response.body
+
+    expect(expected).to.not.be.undefined()
+    expect(expected).to.have.property('id')
+  })
+
+  it('Should be thrown error if try donate in same incident POST (/donations)', async () => {
+    const body = {
+      incident_id: incident.id,
+    }
+
+    const response = await agent
+      .post('/donations')
+      .send(body)
+      .set('Authorization', `bearer ${donorToken}`)
+
+    const expected = response.body
+
+    expect(expected).to.have.property('message')
+  })
+
+  it('Should be to list all donations GET (/donations)', async () => {
+    const response = await agent.get('/donations').send()
+
+    const expected = response.body
+
+    expect(expected).to.not.be.undefined()
+    expect(expected[0]).to.have.property('id')
+    expect(expected[0]).to.have.property('incident_id')
+    expect(expected[0]).to.have.property('donor_id')
+  })
+
+  it('Should be to return empty list donations with fake ong_id GET (/donations?ong_id=)', async () => {
+    const response = await agent.get('/donations?ong_id=FAKE ONG ID').send()
+
+    const expected = response.body
+
+    expect(expected).to.not.be.undefined()
+    expect(expected).to.have.lengthOf(0)
+  })
+
+  it('Should be to list donations by ong_id GET (/donations?ong_id=)', async () => {
+    const response = await agent.get(`/donations?ong_id=${ong.id}`).send()
+
+    const expected = response.body
+
+    expect(expected).to.not.be.undefined()
+    expect(expected[0]).to.have.property('id')
+    expect(expected[0]).to.have.property('incident_id')
+    expect(expected[0]).to.have.property('donor_id')
+    expect(expected[0]).to.have.property('ong_id')
+  })
+})
--- __tests__/integration/incidents.routes.spec.ts
+++ __tests__/integration/incidents.routes.spec.ts
@@ -7,6 +7,7 @@ import { app } from '@app'
 import { Ong } from '@entities/Ong'
 
 import {
+  mock,
   createAgent,
   createFakeDonation,
   createFakeDonor,
@@ -15,186 +16,170 @@ import {
   loginWithFakeDonor,
   loginWithFakeOng,
   createFakeIncident
-} from './fakes/stubs'
+} from './fakes/mocks'
 
 use(dirty)
 
-export function incidents(): void {
-  describe('Incidents Routes', () => {
-    let incident_id: string
-    let ong: Ong
-    let ongToken: string
-    let agent: SuperTest<Test>
+describe('Incidents Routes', () => {
+  let incident_id: string
+  let ong: Ong
+  let ongToken: string
+  let agent: SuperTest<Test>
 
-    before(async () => {
-      await createTestingConnection()
-      agent = createAgent(app)
-      ong = await createFakeOng(agent)
-      ongToken = await loginWithFakeOng(agent)
-    })
-
-    it('Should be able to throw an error if ong_id is fake POST (/incidents)', async () => {
-      const body = {
-        name: 'Some a name',
-        cost: 100.5,
-        description: 'This is a description'
-      }
-
-      await agent.post('/incidents').send(body).expect(401)
-    })
-
-    it('Should be able to create new Incident POST (/incidents)', async () => {
-      const body = {
-        name: 'Some a name',
-        cost: 100.5,
-        description: 'This is a description'
-      }
-
-      const response = await agent
-        .post('/incidents')
-        .send(body)
-        .set('Authorization', `bearer ${ongToken}`)
-
-      const expected = response.body
-
-      incident_id = expected.id
-
-      expect(expected).to.not.be.undefined()
-      expect(expected).to.have.property('id')
-      expect(expected).to.have.property('created_at')
-      expect(expected).to.have.property('updated_at')
-    })
-
-    it('Should be able list incidents GET (/incidents)', async () => {
-      const { id: ong_id } = await createFakeOng(agent)
-
-      const ongToken = await loginWithFakeOng(agent)
-
-      await createFakeIncident(agent, {
-        ongToken,
-        ong_id
-      })
-
-      const response = await agent
-        .get('/incidents')
-        .set('Authorization', `bearer ${ongToken}`)
-
-      const expected = response.body
-
-      expect(expected[0]).to.have.property('id')
-      expect(expected[0]).to.have.property('created_at')
-      expect(expected[0]).to.have.property('updated_at')
-    })
-
-    it('Should be able list incidents by ong id GET (/incidents?ong_id=)', async () => {
-      const response = await agent
-        .get(`/incidents?ong_id=${ong.id}`)
-        .set('Authorization', `bearer ${ongToken}`)
-
-      const expected = response.body
-
-      expect(expected[0]).to.have.property('id')
-      expect(expected[0]).to.have.property('created_at')
-      expect(expected[0]).to.have.property('updated_at')
-    })
-
-    it('Should be able list one Incident by Id GET (/incidents/:id)', async () => {
-      const response = await agent
-        .get(`/incidents/${incident_id}`)
-        .set('Authorization', `bearer ${ongToken}`)
-
-      const expected = response.body
-
-      expect(expected).to.have.property('id')
-      expect(expected).to.have.property('created_at')
-      expect(expected).to.have.property('updated_at')
-    })
-
-    it('Should be able update one Incident by Id PATCH (/incidents/:id)', async () => {
-      const body = {
-        name: 'Some a name updated',
-        cost: 100,
-        description: 'This is a description updated'
-      }
-
-      const response = await agent
-        .patch(`/incidents/${incident_id}`)
-        .send(body)
-        .set('Authorization', `bearer ${ongToken}`)
-
-      const expected = response.body
-
-      expect(expected.name).to.be.equal('Some a name updated')
-      expect(expected.cost).to.be.equal(100)
-      expect(expected.description).to.be.equal('This is a description updated')
-    })
-
-    it('Should be able delete one Incident by Id DELETE (/incidents/:id)', async () => {
-      const response = await agent
-        .delete(`/incidents/${incident_id}`)
-        .set('Authorization', `bearer ${ongToken}`)
-
-      const expected = response.body
-
-      expect(expected).to.have.property('id')
-      expect(expected).to.have.property('created_at')
-      expect(expected).to.have.property('updated_at')
-    })
-
-    it('Should be able list non-donated incidents GET (/incidents?donated=false)', async () => {
-      const ongToken = await loginWithFakeOng(agent)
-
-      await createFakeIncident(agent, {
-        ong_id: ong.id,
-        ongToken
-      })
-
-      const response = await agent
-        .get('/incidents?donated=false')
-        .set('Authorization', `bearer ${ongToken}`)
-
-      const incidents = response.body
-
-      const nullableDonationsIds = []
-
-      for (const incident of incidents) {
-        nullableDonationsIds.push(incident.donation_id)
-      }
-
-      const isNonDonatedIncidents = nullableDonationsIds.includes(null)
-
-      expect(isNonDonatedIncidents).to.be.true()
-    })
-
-    it('Should be able list donated incidents GET (/incidents?donated=true)', async () => {
-      await createFakeDonor(agent)
-
-      const ongToken = await loginWithFakeOng(agent)
-      const { id: incident_id } = await createFakeIncident(agent, {
-        ong_id: ong.id,
-        ongToken
-      })
-
-      await createFakeDonation(agent, {
-        incident_id,
-        donorToken: await loginWithFakeDonor(agent)
-      })
-
-      const response = await agent
-        .get('/incidents?donated=true')
-        .set('Authorization', `bearer ${ongToken}`)
-
-      const incidents = response.body
-
-      const nullableDonationsIds = []
-
-      for (const incident of incidents) {
-        nullableDonationsIds.push(incident.donation_id)
-      }
-
-      const isNonDonatedIncidents = nullableDonationsIds.includes(null)
-
-      expect(isNonDonatedIncidents).to.be.false()
-    })
+  before(async () => {
+    console.log(mock.incident)
+    await createTestingConnection()
+    agent = createAgent(app)
+    ong = await createFakeOng(agent)
+    ongToken = await loginWithFakeOng(agent)
+    console.log('ongToken', ongToken)
   })
-}
+
+  it('Should be able to throw an error if ong_id is fake POST (/incidents)', async () => {
+    await agent.post('/incidents').send(mock.incident).expect(401)
+  })
+
+  it('Should be able to create new Incident POST (/incidents)', async () => {
+    const response = await agent
+      .post('/incidents')
+      .send(mock.incident)
+      .set('Authorization', `bearer ${ongToken}`)
+
+    const expected = response.body
+    console.log(expected)
+
+    incident_id = expected.id
+
+    expect(expected).to.not.be.undefined()
+    expect(expected).to.have.property('id')
+    expect(expected).to.have.property('created_at')
+    expect(expected).to.have.property('updated_at')
+  })
+
+  it('Should be able list incidents GET (/incidents)', async () => {
+    const { id: ong_id } = ong
+
+    await createFakeIncident(agent, {
+      ongToken,
+      ong_id
+    })
+
+    const response = await agent
+      .get('/incidents')
+      .set('Authorization', `bearer ${ongToken}`)
+
+    const expected = response.body
+
+    expect(expected[0]).to.have.property('id')
+    expect(expected[0]).to.have.property('created_at')
+    expect(expected[0]).to.have.property('updated_at')
+  })
+
+  it('Should be able list incidents by ong id GET (/incidents?ong_id=)', async () => {
+    const response = await agent
+      .get(`/incidents?ong_id=${ong.id}`)
+      .set('Authorization', `bearer ${ongToken}`)
+
+    const expected = response.body
+
+    expect(expected[0]).to.have.property('id')
+    expect(expected[0]).to.have.property('created_at')
+    expect(expected[0]).to.have.property('updated_at')
+  })
+
+  it('Should be able list one Incident by Id GET (/incidents/:id)', async () => {
+    const response = await agent
+      .get(`/incidents/${incident_id}`)
+      .set('Authorization', `bearer ${ongToken}`)
+
+    const expected = response.body
+
+    expect(expected).to.have.property('id')
+    expect(expected).to.have.property('created_at')
+    expect(expected).to.have.property('updated_at')
+  })
+
+  it('Should be able update one Incident by Id PATCH (/incidents/:id)', async () => {
+    const body = {
+      name: 'Some a name updated',
+      cost: 100,
+      description: 'This is a description updated'
+    }
+
+    await agent
+      .patch(`/incidents/${incident_id}`)
+      .send(body)
+      .set('Authorization', `bearer ${ongToken}`)
+
+    const { body: expected } = await agent.get(`/incidents/${incident_id}`)
+      .set('Authorization', `bearer ${ongToken}`)
+
+    expect(expected.name).to.be.equal('Some a name updated')
+    expect(expected.cost).to.be.equal(100)
+    expect(expected.description).to.be.equal('This is a description updated')
+  })
+
+  it('Should be able delete one Incident by Id DELETE (/incidents/:id)', async () => {
+    const response = await agent
+      .delete(`/incidents/${incident_id}`)
+      .set('Authorization', `bearer ${ongToken}`)
+
+    const expected = response.body
+
+    expect(expected).to.have.property('id')
+    expect(expected).to.have.property('created_at')
+    expect(expected).to.have.property('updated_at')
+  })
+
+  it('Should be able list non-donated incidents GET (/incidents?donated=false)', async () => {
+    await createFakeIncident(agent, {
+      ong_id: ong.id,
+      ongToken
+    })
+
+    const response = await agent
+      .get('/incidents?donated=false')
+      .set('Authorization', `bearer ${ongToken}`)
+
+    const incidents = response.body
+
+    const nullableDonationsIds = []
+
+    for (const incident of incidents) {
+      nullableDonationsIds.push(incident.donation_id)
+    }
+
+    const isNonDonatedIncidents = nullableDonationsIds.includes(null)
+
+    expect(isNonDonatedIncidents).to.be.true()
+  })
+
+  it('Should be able list donated incidents GET (/incidents?donated=true)', async () => {
+    const { id: incident_id } = await createFakeIncident(agent, {
+      ong_id: ong.id,
+      ongToken
+    })
+
+    await createFakeDonor(agent)
+    await createFakeDonation(agent, {
+      incident_id,
+      donorToken: await loginWithFakeDonor(agent)
+    })
+
+    const response = await agent
+      .get('/incidents?donated=true')
+      .set('Authorization', `bearer ${ongToken}`)
+
+    const incidents = response.body
+
+    const nullableDonationsIds = []
+
+    for (const incident of incidents) {
+      nullableDonationsIds.push(incident.donation_id)
+    }
+
+    const isNonDonatedIncidents = nullableDonationsIds.includes(null)
+
+    expect(isNonDonatedIncidents).to.be.false()
+  })
+})
