de0983a6da322260db87c3bce34348f8b3094ade
HallexCosta <hallex.costa@hotmail.com>
2021-10-26 22:30:19 -0300

    chore(mocha): not allow uncaught

--- .mocharc.integration.js
+++ .mocharc.integration.js
@@ -2,6 +2,8 @@ const config = require('./.mocharc.global')
 
 module.exports = {
   ...config,
+  bail: true,
+  'allow-uncaught': false,
   exit: true,
   file: ['__tests__/setup.ts'],
   spec: ['__tests__/integration/**/*.spec.ts'],
