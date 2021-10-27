adabd551929778caf706426d9ba56f14700d9881
HallexCosta <hallex.costa@hotmail.com>
2021-10-26 22:21:34 -0300

    chore(mocha): force exit in configs mocharc integration

--- .mocharc.integration.js
+++ .mocharc.integration.js
@@ -2,6 +2,7 @@ const config = require('./.mocharc.global')
 
 module.exports = {
   ...config,
+  exit: true,
   file: ['__tests__/setup.ts'],
   spec: ['__tests__/integration/**/*.spec.ts'],
   exclude: ['__tests__/integration/isolate/*.spec.ts']
