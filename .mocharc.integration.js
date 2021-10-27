6204e116bb7ecccd43e1af882d2e3110b4ddebf5
HallexCosta <hallex.costa@hotmail.com>
2021-10-26 23:11:04 -0300

    chore(mocha): fail to zero like true

--- .mocharc.integration.js
+++ .mocharc.integration.js
@@ -4,6 +4,7 @@ module.exports = {
   ...config,
   bail: true,
   'allow-uncaught': false,
+  'fail-zero': true,
   exit: true,
   file: ['__tests__/setup.ts'],
   spec: ['__tests__/integration/**/*.spec.ts'],
