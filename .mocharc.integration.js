d968f3b271126d78845834fc4e65514d2f89f311
HallexCosta <hallex.costa@hotmail.com>
2021-10-27 00:31:44 -0300

    chore(mocha): move some configs for global scope

--- .mocharc.integration.js
+++ .mocharc.integration.js
@@ -2,10 +2,6 @@ const config = require('./.mocharc.global')
 
 module.exports = {
   ...config,
-  bail: true,
-  'allow-uncaught': false,
-  'fail-zero': true,
-  exit: true,
   file: ['__tests__/setup.ts'],
   spec: ['__tests__/integration/**/*.spec.ts'],
   exclude: ['__tests__/integration/isolate/*.spec.ts']
