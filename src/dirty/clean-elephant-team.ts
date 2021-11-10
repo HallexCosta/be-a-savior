0432ec94de4edcfaaaa75bcc252222520aa087f4
HallexCosta <hallex.costa@hotmail.com>
2021-11-10 01:24:53 -0300

    chore(dirty): adjust clean elephant instances for team

--- src/dirty/clean-elephant-team.ts
+++ src/dirty/clean-elephant-team.ts
@@ -1,15 +1,28 @@
-import { ElephantSQLInstanceProvider } from '@providers/elephant/ElephantSQLInstanceProvider'
+import { ElephantSQLInstanceProvider, ElephantListInstance } from '@providers/elephant/ElephantSQLInstanceProvider'
+import axios from 'axios'
 
 const apikey = process.env.ELEPHANT_API_KEY
 const provider = new ElephantSQLInstanceProvider(apikey)
 
+export const api = axios.create({
+  baseURL: 'https://customer.elephantsql.com/api'
+})
+api.interceptors.request.use(configs => {
+  configs.auth = {
+    username: null,
+    password: apikey
+  }
+  return configs
+})
 async function load() {
   console.log('load apikey', apikey)
 
-  const instances = await provider.listInstances()
+  //const instances = await provider.listInstances()
+  const { data: instances } = await api.get<ElephantListInstance[]>('/instances')
+  //console.log(instances)
   for (const instance of instances) {
-    const deleted = await provider.deleteInstance(instance.name)
-    console.log(instance.name, deleted)
+    await api.delete(`/instances/${instance.id}`)
+    console.log(instance.name, true)
   }
 }
 
