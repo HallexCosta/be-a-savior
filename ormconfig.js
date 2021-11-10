0f48f63c2024b2b18c6d9e6f7fff8cbcb945427c
HallexCosta <hallex.costa@hotmail.com>
2021-11-09 23:51:55 -0300

    chore(workflows): fix

--- ormconfig.js
+++ ormconfig.js
@@ -3,12 +3,12 @@ class DB {
     environment = environment.toUpperCase()
 
     this.config = {
-      type: process.env[`DB_TYPE`],
-      host: process.env[`DB_HOST`],
-      port: process.env[`DB_PORT`],
-      username: process.env[`DB_USERNAME`],
-      password: process.env[`DB_PASSWORD`],
-      database: process.env[`DB_NAME`],
+      type: process.env.DB_TYPE,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     }
   }
 }
