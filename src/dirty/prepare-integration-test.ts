ee2410752dcb1debeed6ed4aa6ebc9b1e7120812
HallexCosta <hallex.costa@hotmail.com>
2021-11-25 18:11:43 -0300

    chore(dirty): rewrite .env only docker container

--- src/dirty/prepare-integration-test.ts
+++ src/dirty/prepare-integration-test.ts
@@ -86,8 +86,10 @@ async function prepareEnvironment(environment: string = null) {
       content.push(`${key}=${value}`)
     }
 
-    await fs.writeFile('.env.bkp', await fs.readFile('.env'))
-    await fs.writeFile('.env', content.join('\n'))
+    if (process.env.DOCKER_CONTAINER) {
+      await fs.writeFile('.env.bkp', await fs.readFile('.env'))
+      await fs.writeFile('.env', content.join('\n'))
+    }
 
     console.log('> Override db configs: %s', environment.toLocaleLowerCase())
     const env = dotenv.parse(await fs.readFile(path.resolve(process.cwd(), '.env')))
