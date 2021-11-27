9553c14faaf120c3b287e029f14198ba58e9116b
HallexCosta <hallex.costa@hotmail.com>
2021-11-27 16:27:33 -0300

    test(integration): fix test ongs

--- __tests__/integration/ongs.routes.spec.ts
+++ __tests__/integration/ongs.routes.spec.ts
@@ -6,6 +6,7 @@ import faker from 'faker'
 import { app } from '@app'
 
 import { mock, createTestingConnection, createAgent } from './fakes/mocks'
+import { Util } from './util'
 
 faker.locale = 'pt_BR'
 
@@ -16,13 +17,8 @@ describe('Ongs Routes', () => {
   let agent: SuperTest<Test>
 
   before(async () => {
-    try {
-      await createTestingConnection()
-      agent = createAgent(app)
-    } catch (e) {
-      console.log(e)
-      throw e
-    }
+    await createTestingConnection()
+    agent = createAgent(app)
   })
 
   it('Should be able to create new Ong POST (/ongs)', async () => {
@@ -41,30 +37,33 @@ describe('Ongs Routes', () => {
   it('Should be able authenticate ong POST (/ongs/login)', async () => {
     const { email, password } = mock.ong
     const response = await agent.post('/ongs/login').send({ email, password })
-    const expected = response.body
+    const { token } = response.body
 
-    expect(expected).to.have.property('token')
-    expect(expected).to.not.be.undefined()
+    const tokenDecrypted = Util.decryptJWTToken(token)
+    const tokenParsed = JSON.parse(tokenDecrypted)
+
+    expect(tokenParsed.email).to.be.equal(email)
+    expect(tokenParsed).to.have.property('exp')
   })
 
   it('Should be able list Ongs GET (/ongs)', async () => {
     const response = await agent.get('/ongs')
 
-    const expected = response.body
+    const ongs = response.body
 
-    expect(expected[0]).to.have.property('id')
-    expect(expected[0]).to.have.property('created_at')
-    expect(expected[0]).to.have.property('updated_at')
+    expect(ongs[0]).to.have.property('id')
+    expect(ongs[0]).to.have.property('created_at')
+    expect(ongs[0]).to.have.property('updated_at')
   })
 
   it('Should be able list one Ong by Id GET (/ongs/:id)', async () => {
     const response = await agent.get(`/ongs/${id}`)
 
-    const expected = response.body
+    const ong = response.body
 
-    expect(expected).to.have.property('id')
-    expect(expected.id).to.be.equal(id)
-    expect(expected).to.have.property('created_at')
-    expect(expected).to.have.property('updated_at')
+    expect(ong).to.have.property('id')
+    expect(ong.id).to.be.equal(id)
+    expect(ong).to.have.property('created_at')
+    expect(ong).to.have.property('updated_at')
   })
 })
