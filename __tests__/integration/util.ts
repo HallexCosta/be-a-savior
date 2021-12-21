0a66a493a72c3f28abe55d3828d0c463b03564c3
hallexcosta <hallex.costa@hotmail.com>
2021-12-21 00:11:28 -0300

    chore(integration): add static prop to store customers email

--- __tests__/integration/util.ts
+++ __tests__/integration/util.ts
@@ -1,4 +1,5 @@
 export class Util {
+  static customersEmail = []
   static decryptJWTToken(token: string) {
     const [, body] = token.split('.')
     return Buffer.from(body, 'base64').toString('utf-8')
