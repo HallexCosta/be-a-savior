a65c9122de7a0a9b5dae5c5c1437e1644ef4309b
hallexcosta <hallex.costa@hotmail.com>
2022-01-07 18:38:12 -0300

    feat(services): add LIstUsersService

---
+++ src/services/users/ListUsersService.ts
@@ -0,0 +1,41 @@
+import { classToClass } from "class-transformer"
+
+import { User } from '@entities/User'
+
+import { UsersRepository } from '@repositories/UsersRepository'
+
+import { BaseUserService } from '@services/users/BaseUserService'
+
+type ListUsersServiceDependencies = {
+  repositories: {
+    users: UsersRepository
+  }
+}
+
+type ListUsersDTO = {
+  owner: string
+}
+
+type ListUsersExecuteParams = {
+  dto: ListUsersDTO
+}
+
+export abstract class ListUsersService extends BaseUserService {
+  public constructor(listUsersServiceDependencies: ListUsersServiceDependencies) {
+    super(listUsersServiceDependencies)
+  }
+
+  public async executeUser<ListUsersResponse = User>({
+    dto: {
+      owner
+    }
+  }: ListUsersExecuteParams): Promise<ListUsersResponse> {
+    const usersRepository = this.repositories.users
+
+    const users = await usersRepository.findByOwner(owner)
+
+    const usersResponse = users.map(user => classToClass<User>(user)) as unknown as ListUsersResponse
+
+    return usersResponse
+  }
+}
