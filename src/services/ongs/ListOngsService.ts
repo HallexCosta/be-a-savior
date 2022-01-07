d52ea286b937d8893980c846a6109619a37b58f0
hallexcosta <hallex.costa@hotmail.com>
2022-01-07 18:39:26 -0300

    chore(services): use ListUsersService

--- src/services/ongs/ListOngsService.ts
+++ src/services/ongs/ListOngsService.ts
@@ -1,19 +1,30 @@
-import { classToClass } from 'class-transformer'
-import { getCustomRepository } from 'typeorm'
-
 import { Ong } from '@entities/Ong'
 import { UsersRepository } from '@repositories/UsersRepository'
 
+import { ListUsersService } from '@services/users/ListUsersService'
+
+type ListOngsServiceDependencies = {
+  repositories: {
+    users: UsersRepository
+  }
+}
+
+type ListOngDTO = {
+  owner?: string
+}
+
 type ListOngsResponse = Omit<Ong, 'password'>[]
 
-export class ListOngsService {
-  public async execute(): Promise<ListOngsResponse> {
-    const ongsRepository = getCustomRepository(UsersRepository)
+export class ListOngsService extends ListUsersService {
+  public constructor(listOngsServiceDependencies: ListOngsServiceDependencies) {
+    super(listOngsServiceDependencies)
+  }
 
-    const ongs = await ongsRepository.find()
-
-    const ongsResponse = classToClass<ListOngsResponse>(ongs)
-
-    return ongsResponse
+  public async execute({ owner }: ListOngDTO): Promise<ListOngsResponse> {
+    return await super.executeUser({
+      dto: {
+        owner
+      }
+    })
   }
 }
