57c66d5f522f7b74bc24b0ca22ee651663c3c05d
HallexCosta <hallex.costa@hotmail.com>
2022-02-16 17:14:23 -0200

    chore(app): add log to start app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata'
 import express from 'express'
 import 'express-async-errors'
+import logger from '@common/logger'
 
 import './database'
 
@@ -9,6 +10,9 @@ import { handleErrors } from '@middlewares/handleErrors'
 import { routes } from '@routes'
 
 const app = express()
+const appName = process.env.APPNAME
+
+logger.info(`Starting Application: ${appName}`)
 
 app.use(express.json())
 app.use(routes)
