77b80f483f3e4a789f63985dc45759d3f154f667
HallexCosta <hallex.costa@hotmail.com>
2022-02-25 20:57:57 -0300

    chore(services): throw message if try delete incident with donations

--- src/services/incidents/DeleteIncidentService.ts
+++ src/services/incidents/DeleteIncidentService.ts
@@ -1,4 +1,4 @@
-import { DeleteResult, getCustomRepository } from 'typeorm'
+import { DeleteResult } from 'typeorm'
 
 import { Incident } from '@entities/Incident'
 import { IncidentsRepository } from '@repositories/IncidentsRepository'
@@ -8,21 +8,42 @@ type DeleteIncidentDTO = {
   ongId: string
 }
 
+export type DeleteIncidentRepositories = {
+  incidents: IncidentsRepository
+}
+
+export type DeleteIncidentDependencies = {
+  repositories: DeleteIncidentRepositories
+}
+
 export class DeleteIncidentService {
+  private readonly repositories: DeleteIncidentRepositories
+
+  public constructor(deleteIncidentDependencies: DeleteIncidentDependencies) {
+    Object.assign(this, deleteIncidentDependencies)
+  }
+
   public async execute({ id, ongId }: DeleteIncidentDTO): Promise<Incident> {
-    const repository = getCustomRepository(IncidentsRepository)
+    const repository = this.repositories.incidents
 
     const incident = await repository.findById(id)
 
     this.checkIncidentExists(incident)
-
     this.checkIncidentBelongsThisOng(ongId, incident.user_id)
+    this.checkIncidentsHaveDonations(incident)
 
     const result = await repository.deleteById(id)
 
     this.checkIncidentDeleteWithSuccess(result)
 
     return incident
+  }
+
+  public checkIncidentsHaveDonations(incident: Incident): void {
+    const haveDonations = incident.donations.length > 0
+
+    if (haveDonations)
+      throw new Error("It isn't possible delete incident that have donations")
   }
 
   private checkIncidentBelongsThisOng(
