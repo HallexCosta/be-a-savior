20e28c0f347350e903cc1aea8197549c0f8d39d4
HallexCosta <hallex.costa@hotmail.com>
2022-07-11 17:58:25 -0300

    test(unit): add unit test services incident update incident cost
    Should prevent ONG of edit the incident cost if the cost of incident is lower than the donations total

---
+++ __tests__/unit/services/incidents/updateIncidentService.spec.ts
@@ -0,0 +1,48 @@
+import dirtyChai from 'dirty-chai'
+import { expect, use } from 'chai'
+
+import { UpdateIncidentService } from '@services/incidents/UpdateIncidentService'
+import { ServiceDependencies } from '@services/BaseService'
+
+import { IncidentsRepository } from '@repositories/IncidentsRepository'
+
+import { Incident } from '@entities/Incident'
+
+use(dirtyChai)
+
+describe('@UpdateIncidentService', () => {
+  describe('#preventUpdateIncidentCost', () => {
+    it('should prevent ONG of edit the incident cost if the cost of incident is lower than the donations total', () => {
+      const incidentsRepository = {
+        findById: () => { }
+      } as unknown as IncidentsRepository
+
+      const updateIncidentServiceDependenciesMock = {
+        repositories: {
+          incidents: incidentsRepository
+        }
+      } as ServiceDependencies
+
+      const updateIncidentService = new UpdateIncidentService(updateIncidentServiceDependenciesMock)
+
+      const toThrow = () => {
+        const incident = {
+          cost: 1000,
+          donations: [
+            {
+              amount: 5000
+            },
+            {
+              amount: 5000
+            }
+          ]
+        } as Incident
+
+        const incidentCostUpdated = 5000
+        updateIncidentService.preventUpdateIncidentCost(incidentCostUpdated, incident.donations)
+      }
+
+      expect(toThrow).to.be.throw("Opss... can't possible update incident cost that reached max limit of donations")
+    })
+  })
+})
