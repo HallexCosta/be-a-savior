c78d483b701b0d235174ba8c3f0ce89eabd602ae
HallexCosta <hallex.costa@hotmail.com>
2022-08-22 00:12:42 -0300

    tests(unit): add test to method execute from ListIncidentsService

--- __tests__/unit/services/ListIncidentsService.spec.ts
+++ __tests__/unit/services/ListIncidentsService.spec.ts
@@ -6,25 +6,83 @@ import { createMocks } from '@tests/fakes/mocks'
 import { Incident } from '@entities/Incident'
 
 import { ListIncidentsService, ListIncidentsDependencies } from '@services/incidents/ListIncidentsService'
+import { Donation } from '@entities/Donation'
 
 describe('@ListIncidentsService', () => {
   let sandbox: sinon.SinonSandbox
   const defaultFunction = () => { }
   const listIncidentsDependenciesMock = {
     repositories: {
-      users: {
+      incidents: {
         findIncidentsByFilter: defaultFunction
       },
       providers: {}
-    }
-  }
+    } 
+  } as unknown as ListIncidentsDependencies
 
   describe('#constructor', () => {
     beforeEach(() => {
       sandbox = sinon.createSandbox()
     })
     afterEach(() => sandbox.restore())
+  })
 
+  describe('#execute', () => {
+    beforeEach(() => {
+      sandbox = sinon.createSandbox()
+    })
+    afterEach(() => sandbox.restore())
+
+    it('Should be list all incidents with filters passed', async () => {
+      const listIncidentInstance = new ListIncidentsService(
+        listIncidentsDependenciesMock
+      )
+
+      const params = {
+        ongId: '',
+        donorId: '',
+        donated: true
+      }
+      const donationsMock = (Array.from(Array(4).keys())).map(() => {
+        const donation = {
+          ...createMocks().donation,
+          amount: 1000
+        } as Donation
+        return donation
+      })
+      const incidentsMocks = (Array.from(Array(4).keys())).map(() => {
+        const incident = {
+          ...createMocks().incident,
+          donations: donationsMock
+        } as Incident
+        return  incident 
+      })
+
+      const findIncidentsByFilterStubbed = sandbox.stub(
+        listIncidentsDependenciesMock.repositories.incidents,
+        'findIncidentsByFilter'
+      ).resolves(incidentsMocks)
+
+      const totalIncidentMock = 4
+      const totalIncidentsAndDonationsReturnsMock = {
+        totalIncidents: totalIncidentMock,
+        totalDonations: totalIncidentMock * 4,
+        totalIncidentsDonated: 4,
+        totalIncidentsNonDonated: 0
+      }
+      const totalIncidentsAndDonationsStubbed = sandbox.stub(
+        listIncidentInstance,
+        'totalIncidentsAndDonations'
+      ).returns(totalIncidentsAndDonationsReturnsMock)
+
+      const listIncidentsData = await listIncidentInstance.execute(params)
+
+      expect(findIncidentsByFilterStubbed.calledWith(params)).to.be.true("doesn't called with params correct")
+      expect(totalIncidentsAndDonationsStubbed.calledOnce).to.be.true("doesn't possible to calculate total of incidents and donations")
+      expect(listIncidentsData.incidents).to.be.lengthOf(4, "The return don't receive length expected")
+      expect(listIncidentsData.totalIncidentsAndDonations)
+        .to.deep.include(totalIncidentsAndDonationsStubbed, 'Failed to match return of the object from totalIncidentsAndDonations')
+    })
   })
 
   describe('#totalIncidentsAndDonations', () => {
