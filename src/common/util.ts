4b43a9b82cd7e01195167e2c900a1baafa4a4d16
HallexCosta <hallex.costa@hotmail.com>
2022-08-28 20:00:44 -0300

    chore(common): add new method switch in util

--- src/common/util.ts
+++ src/common/util.ts
@@ -1,17 +1,38 @@
-import { classToClass } from "class-transformer"
+import { classToClass } from 'class-transformer'
 
-export class Util {
+export declare type SwitchParams = {
+  entry: string
+  cases: {
+    [k: string]: Function
+  }
+}
+
+export type Case = {
+  name: string
+  then: Function
+}
+
+export default class Util {
   static classesToClasses<T>(datas: T[]) {
-    return datas.map(data => classToClass<T>(data))
+    return datas.map((data) => classToClass<T>(data))
   }
 
   static isUUID(uuid: string) {
-    const regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+    const regex =
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
     return regex.test(uuid)
   }
+
   static delay(ms: number) {
     return new Promise((resolve) => {
       setTimeout(resolve, ms)
     })
+  }
+
+  static switch({ entry, cases }: SwitchParams) {
+    const treatment = cases[entry]
+    try {
+      treatment()
+    } catch(e) {}
   }
 }
