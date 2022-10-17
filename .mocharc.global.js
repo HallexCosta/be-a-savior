df69c8f66ba4a66c0a98ba1f3730dae0a6f08ddb
HallexCosta <hallex.costa@hotmail.com>
2022-10-17 01:56:16 -0300

    feat(scripts): add script "test:integration:ci" to run tests with bail true

--- .mocharc.global.js
+++ .mocharc.global.js
@@ -3,11 +3,16 @@ module.exports = {
   diff: true,
   color: true,
   exit: true,
-  bail: true,
+  bail: false,
   extension: ['ts'],
   package: './package.json',
   reporter: 'spec',
-  require: ['make-promises-safe', 'dotenv/config', 'ts-node/register', 'tsconfig-paths/register'],
+  require: [
+    'make-promises-safe',
+    'dotenv/config',
+    'ts-node/register',
+    'tsconfig-paths/register'
+  ],
   timeout: 0,
-  ui: 'bdd',
+  ui: 'bdd'
 }
