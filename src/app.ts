a84d01a645858097387e5d4f118f600437fb0398
HallexCosta <hallex.costa@hotmail.com>
2022-11-05 16:22:19 -0300

    feat: import database if app is not running in cluster mode

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,13 @@ import { handleErrors } from '@middlewares/handleErrors'
 
 import { routes } from '@routes'
 
+// import database if app is not running in cluster mode
+if (!process.env.MODE || !process.env.MODE.includes('cluster')) {
+  (async () => {
+    await import('./database')
+  })()
+}
+
 const app = express()
 const appName = process.env.APPNAME
 
